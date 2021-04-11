bb38d0afabcd9b2040a894269768f3bc3136dfb0
Adilson Lustoza <adilsonlustoza@gmail.com>
2021-04-11 19:59:39 -0300

    ajustes no front

--- AppPrivy.WebAppSiteBlog/obj/Release/net5.0/win-x64/PubTmp/Out/wwwroot/js/views/Contato.js
+++ AppPrivy.WebAppSiteBlog/obj/Release/net5.0/win-x64/PubTmp/Out/wwwroot/js/views/Contato.js
@@ -5,7 +5,6 @@ $('.btn.btn-outline-primary').on('click', function () {
     ajaxObjects.sendPost(url, 'form', true, '.btn.btn-primary');
 });
 
-
 $("#Telefone")
     .mask("(99) 9999-9999?9")
     .focusout(function (event) {
@@ -21,10 +20,42 @@ $("#Telefone")
         }
     });
 
-    $('input,textarea').on('keyup blur change click submit',function()
-    {  
-       if( $(this).attr('aria-invalid')==='true')    
-           $(this).removeClass('is-valid').addClass('is-invalid');
-      else if( $(this).attr('aria-invalid')==='false')    
-           $(this).removeClass('is-invalid').addClass('is-valid');
-    });
+const validMark = (obj) => {
+
+    try {
+
+        let arrayFields = $(obj).serializeArray();
+
+        if ($.isArray(arrayFields) && arrayFields.length > 0)
+        {
+            $.each(arrayFields, (_index, field) => {
+                let _field = '#' + field["name"];
+                if ($(_field).attr('aria-invalid') === 'true' || $(_field).val() === '')
+                    $(_field).removeClass('is-valid').addClass('is-invalid');
+                else if ($(_field).attr('aria-invalid') === 'false')
+                    $(_field).removeClass('is-invalid').addClass('is-valid');
+            });
+        }
+        else {
+            if ($(obj).is('input') || $(obj).is('textarea')) {
+                if ($(obj).attr('aria-invalid') === 'true' || $(obj).val() === '')
+                    $(obj).removeClass('is-valid').addClass('is-invalid');
+                else if ($(obj).attr('aria-invalid') === 'false')
+                    $(obj).removeClass('is-invalid').addClass('is-valid');
+            }
+        }
+
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+$('input,textarea').on('keyup change blur', (e) => {
+    validMark(e.target);
+});
+
+
+$('form').submit(function (e) {
+    validMark(this);
+});
