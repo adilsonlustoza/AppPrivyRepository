8cc14f5a1a37187d313a2ec8fad58497ecc3879c
Adilson Lustoza <adilsonlustoza@gmail.com>
2021-04-25 17:33:54 -0300

    Implementando documentacao xml

--- AppPrivy.WebApiDoacaoMais/Startup.cs
+++ AppPrivy.WebApiDoacaoMais/Startup.cs
@@ -210,9 +210,9 @@ namespace AppPrivy.WebApiDoacaoMais
 
 
                 // Set the comments path for the Swagger JSON and UI.
-                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                //opt.IncludeXmlComments(xmlPath);
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                opt.IncludeXmlComments(xmlPath);
 
 
 
